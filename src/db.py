8e8cc953e059b0ec20fce04c4e667af49fd0789d
Arthur C. Watkins <archwa@protonmail.ch>
2019-09-09 02:31:31 -0500

    defined db constructor

--- src/db.py
+++ src/db.py
@@ -1,5 +1,15 @@
 from pymongo import MongoClient
-from pprint import pprint
 import env
 
-client = MongoClient
+class db:
+  def __init__(self):
+    __connection_string = ''.join([
+      env.vars['MONGODB_PROTOCOL'],
+      "://",
+      env.vars['MONGODB_USER'],
+      ":",
+      env.vars['MONGODB_PASS'],
+      "@",
+      env.vars['MONGODB_HOST']
+    ])
+    self.client = MongoClient(__connection_string)
