d9d52bb0cd0bb8c240a755eb2bb6dd81ab1939cc
Arthur C. Watkins <archwa@protonmail.ch>
2019-10-29 16:44:07 -0500

    Updates

---
+++ precache-manifest.7e90ed6da28cbbecafd085421115e3ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ae74f4ecd446c487334aad7c32d7c3f",
+    "url": "/holdings/index.html"
+  },
+  {
+    "revision": "57cfdcc8ab538d8b7409",
+    "url": "/holdings/static/css/main.7e661a20.chunk.css"
+  },
+  {
+    "revision": "a0caedb4a310d6c8688b",
+    "url": "/holdings/static/js/2.25409784.chunk.js"
+  },
+  {
+    "revision": "57cfdcc8ab538d8b7409",
+    "url": "/holdings/static/js/main.51c41a2e.chunk.js"
+  },
+  {
+    "revision": "81967618a0a95448b4d2",
+    "url": "/holdings/static/js/runtime-main.ca4dc17d.js"
+  },
+  {
+    "revision": "9942588a6c84f959132556d99e83ded6",
+    "url": "/holdings/static/media/Raleway-Regular.9942588a.ttf"
+  }
+]);
