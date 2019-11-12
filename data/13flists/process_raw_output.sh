bd059c0a5502fefbe9a46076b64450205f19e013
Arthur C. Watkins <archwa@protonmail.ch>
2019-11-12 16:22:58 -0600

    added validation; added count files to keep track of the number of datapoints each 13f list should have; fixed invalid lines in 3 files

--- data/13flists/process_raw_output.sh
+++ data/13flists/process_raw_output.sh
@@ -7,10 +7,12 @@ QTR_CUR=$(((`date +%-m`-1)/3+1))
 for I in `seq ${YR_START} ${YR_CUR}`; do
   for J in `seq 1 4`; do
     if [ ${I} != ${YR_CUR} ] || [ ${J} != ${QTR_CUR} ]; then
-      # remove lines containing field info; strip carriage returns; remove last line with total count info
-      # TODO : perform a check to make sure that the number of lines in the file matches the number on the final line
-      sed -e '/^CUSIP NO.*$/d' -e 's/\r//' -e '$ d' raw_output/tabula-13flist${I}q${J}.csv > inter-13flist${I}q${J}.csv
+      # record claimed number of valid data points
+      claimed=`tail -n1 raw_output/tabula-13flist${I}q${J}.csv | sed -e 's/.*\([0-9]\{2\}\),\?\([0-9]\{3\}\).*/\1\2/'`
 
+      # remove lines containing field info; strip carriage returns; remove last line with total count info
+      sed -e '/^CUSIP NO.*$/d' -e 's/\r//' -e '$ d' raw_output/tabula-13flist${I}q${J}.csv > inter-13flist${I}q${J}.csv
+      
       # insert line containing standardized field info
       sed -i '1s/^/cusip,options,issuer,description,status\n/' inter-13flist${I}q${J}.csv
 
@@ -24,6 +26,18 @@ for I in `seq ${YR_START} ${YR_CUR}`; do
 
       # move formatted output to its corresponding directory
       mv 13flist${I}q${J}.csv ${I}/q${J}/13flist${I}q${J}.csv
+
+      # record actual number of valid datapoints
+      actual=$(((`cat ${I}/q${J}/13flist${I}q${J}.csv | grep "[^,]*,[^,]*,[^,]*,[^,]*,[^,]*" | wc -l`)-1))
+
+      # store claimed number of valid datapoints
+      echo $claimed > ${I}/q${J}/count
+
+      # perform validation; display offending lines
+      if [ $claimed -ne $actual ]; then
+        printf "WARNING: ${I}/q${J}/13flist${I}q${J}.csv claimed $claimed != actual $actual - see offending lines:\n"
+        cat ${I}/q${J}/13flist${I}q${J}.csv | grep -n -v "[^,]*,[^,]*,[^,]*,[^,]*,[^,]*" 
+      fi
     fi
   done
 done
---
+++ data/13flists/validate.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+YR_START=2004
+YR_CUR=`date +"%Y"`
+QTR_CUR=$(((`date +%-m`-1)/3+1))
+
+success=true
+
+for I in `seq ${YR_START} ${YR_CUR}`; do
+  for J in `seq 1 4`; do
+    if [ ${I} != ${YR_CUR} ] || [ ${J} != ${QTR_CUR} ]; then
+      # load claimed number of valid datapoints
+      claimed=`cat ${I}/q${J}/count`
+
+      # record actual number of valid datapoints
+      actual=$(((`cat ${I}/q${J}/13flist${I}q${J}.csv | grep "[^,]*,[^,]*,[^,]*,[^,]*,[^,]*" | wc -l`)-1))
+
+      # perform validation; display offending lines
+      if [ $claimed -ne $actual ]; then
+        success=false
+        printf "FAIL: ${I}/q${J}/13flist${I}q${J}.csv claimed $claimed != actual $actual - see offending lines:\n"
+        cat ${I}/q${J}/13flist${I}q${J}.csv | grep -n -v "[^,]*,[^,]*,[^,]*,[^,]*,[^,]*"
+      fi
+    fi
+  done
+done
+
+if $success; then
+  printf "\nValidation completed with SUCCESS. All files are valid.\n\n"
+else
+  printf "\nValidation completed with FAILURE. See above for offending files.\n\n"
+fi
