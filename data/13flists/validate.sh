889b59d8abecaccecc325e88536df8f704902ee8
Arthur C. Watkins <archwa@protonmail.ch>
2019-11-12 19:21:35 -0600

    fixed validation and processing scripts to handle all cases; extrapolated 2003q4 data from 2004q1 (remove ADDED CUSIPS from 2004q1 to get 2003q4)

--- data/13flists/validate.sh
+++ data/13flists/validate.sh
@@ -5,8 +5,6 @@ YR_CUR=`date +"%Y"`
 QTR_CUR=$(((`date +%-m`-1)/3+1))
 
 # TODO : check that CUSIP is valid (checksum)
-#        check that second position is either empty or has an asterisk or similar
-#        check that last position is either empty, ADDED, or DELETED
 
 success=true
 
@@ -17,13 +15,15 @@ for I in `seq ${YR_START} ${YR_CUR}`; do
       claimed=`cat ${I}/q${J}/count`
 
       # record actual number of valid datapoints
-      actual=$(((`cat ${I}/q${J}/13flist${I}q${J}.csv | grep "[^,]*,[^,]*,[^,]*,[^,]*,[^,]*" | wc -l`)-1))
+      actual=`cat ${I}/q${J}/13flist${I}q${J}.csv | grep -v -E "^cusip,options,issuer,description,status" | grep -E "[^,]{6}\s*[^,]{2}\s*[^,]{1},[*]?,[^,]*,[^,]*,(ADDED|DELETED)?$" | wc -l`
 
-      # perform validation; display offending lines
+      # perform validation; display offending lines (if any)
       if [ $claimed -ne $actual ]; then
         success=false
         printf "FAIL: ${I}/q${J}/13flist${I}q${J}.csv claimed $claimed != actual $actual - see offending lines:\n"
-        cat ${I}/q${J}/13flist${I}q${J}.csv | grep -n -v "[^,]*,[^,]*,[^,]*,[^,]*,[^,]*"
+        cat ${I}/q${J}/13flist${I}q${J}.csv | \
+        grep -v -E "^cusip,options,issuer,description,status" | \
+        grep -n -v -E "[^,]{6}\s*[^,]{2}\s*[^,]{1},[*]?,[^,]*,[^,]*,(ADDED|DELETED)?$"
       fi
     fi
   done
