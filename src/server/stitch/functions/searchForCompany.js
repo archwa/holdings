3983bd8854e11d8cea697f4ae6a629f9be2c3ed9
Arthur C. Watkins <archwa@protonmail.ch>
2019-11-24 12:24:32 -0600

    moved stitch functions to a better-named directory

---
+++ src/server/stitch/functions/searchForCompany.js
@@ -0,0 +1,37 @@
+exports = function(q){
+  /*
+    
+    purpose: search for both filers and issuers
+    
+  */
+  return Promise.all([context.functions.execute('searchForFiler', q), context.functions.execute('searchForIssuer', q)])
+    .then(res => {
+      const filerSearch = res[0];
+      const issuerSearch = res[1];
+      
+      const result = {
+        query: {
+          q,
+          type: "company"
+        },
+        status: 0,
+        message: `Finished compound search (company: filers and issuers).  See \`data\` for more info.`,
+        data: {
+          filerSearch,
+          issuerSearch
+        },
+        filerSearch,
+        issuerSearch
+      };
+      
+      return result;
+    })
+    .catch(err => ({
+      query: {
+        q,
+        type: "company"
+      },
+      status: -1,
+      message: `Error while running \`searchForCompany\`: ${err}`
+    }));
+};
