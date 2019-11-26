a4176234d85ba458cfe909aaff6cb0906afa5736
Arthur C. Watkins <archwa@protonmail.ch>
2019-11-26 08:15:03 -0600

    trying fix for public url

--- src/client/web/src/App.js
+++ src/client/web/src/App.js
@@ -86,7 +86,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <Router basename='/holdings'>
+      <Router basename={process.env.PUBLIC_URL} >
         <div className="App">
           {/*<div className="App-header">
             <h1>Holdings Analysis</h1>
