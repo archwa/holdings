6361389ab6303aaa1807904174cd14e7d4c3ab34
Arthur C. Watkins <archwa@protonmail.ch>
2019-11-26 08:29:10 -0600

    Updates

--- precache-manifest.76b7b8da82ae0a9bf57049939e59bd2d.js
+++ precache-manifest.76b7b8da82ae0a9bf57049939e59bd2d.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "89ca2e9a3ce97838e69b1e7bd38c9f87",
+    "revision": "921538dd07ee8695630edab1c9ce5f58",
     "url": "/holdings/index.html"
   },
   {
-    "revision": "d2557ec8e87bd5708fd2",
+    "revision": "058ca57fecc7d43016e3",
     "url": "/holdings/static/css/main.ed653630.chunk.css"
   },
   {
-    "revision": "18597c4c1d5f6b404f97",
-    "url": "/holdings/static/js/2.25083b09.chunk.js"
+    "revision": "b60b4be8bbf37a8f3c8b",
+    "url": "/holdings/static/js/2.99af18d8.chunk.js"
   },
   {
-    "revision": "d2557ec8e87bd5708fd2",
-    "url": "/holdings/static/js/main.856af50a.chunk.js"
+    "revision": "058ca57fecc7d43016e3",
+    "url": "/holdings/static/js/main.5a1100cb.chunk.js"
   },
   {
     "revision": "81967618a0a95448b4d2",
