02fc3775cb8ebea6f6b4ba7718ce297e48b2476d
Arthur C. Watkins <archwa@protonmail.ch>
2019-11-26 18:28:30 -0600

    Added more background for the tool, as well as some useful definitions

--- src/client/web/src/App.js
+++ src/client/web/src/App.js
@@ -104,13 +104,26 @@ class App extends React.Component {
                 render={
                   (props) => <div className="Welcome">
                     <h2>Welcome to the Holdings Analysis tool!</h2>
-                    <strong>Search tips:</strong>
-                    <ol>
-                      <li>Search tokens match by a logical <strong>OR</strong>.  Ex: <strong>tech media consulting firm</strong> yields results matching any of those words.</li>
-                      <li>To search by exact phrase, use double quotes ({ '"' }).  Ex: <strong>{ '"apple computer"' }</strong> yields results matching <strong>apple computer</strong> EXACTLY.</li>
-                      <li>To exclude a term, prepend that term with a hyphen ({ '-' }).  Ex: <strong>apple -computer</strong> yields results matching <strong>apple</strong> but NOT <strong>computer</strong>.</li>
-                    </ol>
+                    The purpose of this tool is to enable research of both current &amp; historical holders of issued financial securities (holdings).<br/>
+                    This tool uses publicly available data disseminated by the U.S. Securities and Exchange Commission (SEC), the NASDAQ stock exchange, and others.<br/>
+                    To start using the tool, enter a search query using the search bar below.<br/>
+                    <span>You may search either by company (e.g. <strong>Tesla</strong> for Tesla Inc.) or by stock symbol (e.g. <strong>MSFT</strong> for Microsoft Corp.)  (... or <em>both</em> via search query string in the address bar.)</span>
+
+                    <h3>Definitions</h3>
+                    <ul>
+                      <li><strong>Holders</strong> are people, companies, and other entities that own, or <em>hold</em>, a financial security.</li>
+                      <li><strong>Holdings</strong> are collections of financial securities issued to a given holder.</li>
+                      <li><strong>Issuers</strong> are companies that issue, or ascribe ownership of, financial securities to holders.</li>
+                      <li><strong>Filers</strong> are people, companies, and other entities that have filed any form with the SEC.</li>
+                    </ul>
                     
+                    <h3>Search Tips</h3>
+                    <ul>
+                      <li>By default, search tokens match by a logical <em>or</em> operation.  <strong>Example:</strong> The search query <strong>apple computer</strong> yields results matching <strong>apple</strong>, <strong>computer</strong>, or <em>both</em>.</li>
+                      <li>To search by exact phrase, use double quotes ({ '"' }).  <strong>Example:</strong> The search query <strong>{ '"apple computer"' }</strong> yields results matching <strong>apple computer</strong> EXACTLY.</li>
+                      <li>To exclude a term, prepend that term with a hyphen ({ '-' }).  <strong>Example:</strong> The search query <strong>apple -computer</strong> yields results matching <strong>apple</strong> but NOT <strong>computer</strong>.</li>
+                    </ul>
+
                   </div>
                 }
               />
