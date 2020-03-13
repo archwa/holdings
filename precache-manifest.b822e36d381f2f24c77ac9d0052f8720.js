365eb5220d6838c8a6ee04047d8ed6ddb1e2b559
Arthur C. Watkins <archwa@protonmail.ch>
2020-03-13 14:01:03 -0500

    Updates

---
+++ precache-manifest.b822e36d381f2f24c77ac9d0052f8720.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59bb7932f880e425a92cd2ced689c5e9",
+    "url": "/holdings/index.html"
+  },
+  {
+    "revision": "73fa614f43be1c84ee6c",
+    "url": "/holdings/static/css/main.5064ea5e.chunk.css"
+  },
+  {
+    "revision": "99f3bdc2f073726f3bf2",
+    "url": "/holdings/static/js/2.56a8f303.chunk.js"
+  },
+  {
+    "revision": "73fa614f43be1c84ee6c",
+    "url": "/holdings/static/js/main.3ba621fe.chunk.js"
+  },
+  {
+    "revision": "81967618a0a95448b4d2",
+    "url": "/holdings/static/js/runtime-main.ca4dc17d.js"
+  },
+  {
+    "revision": "9942588a6c84f959132556d99e83ded6",
+    "url": "/holdings/static/media/Raleway-Regular.9942588a.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1585bf5ba72729a65cb8da3f35431158.js"
+  "/holdings/precache-manifest.b822e36d381f2f24c77ac9d0052f8720.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/holdings/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
